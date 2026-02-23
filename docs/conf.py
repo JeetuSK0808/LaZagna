5caa1314f40588096a93fa8f263191be60214cc0
IY2002 <ismaely2002@gmail.com>
2026-02-23 18:41:11 -0500

    Updated docs to showcase the new YAML interface.

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+# Mock modules that use internal relative imports (for autodoc on RTD)
+autodoc_mock_imports = [
+    'printing', 'run_interface', 'script_editing', 'arch_xml_modification',
+    'file_handling', 'csv_locations_generator', 'run_flow', 'split_top_module',
+    'yaml_file_processing',
+]
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -22,15 +29,44 @@ extensions = [
     "sphinx.ext.napoleon",      # for Google/NumPy docstrings
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
+    "myst_parser",              # for Markdown support
+]
+
+# Support both .rst and .md source files
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# MyST-Parser configuration
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "fieldlist",
+    "tasklist",
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_nav_header_background': '#2c3e50',
+    'navigation_depth': 4,
+}
+
+# -- Logo configuration -----------------------------------------------------
+html_logo = "_static/LaZagna_logo.png"
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,49 @@
-.. LaZagna documentation master file, created by
-   sphinx-quickstart on Thu Feb 12 20:44:35 2026.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. LaZagna documentation master file
 
-LaZagna documentation
+LaZagna Documentation
 =====================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+.. image:: _static/LaZagna_logo.png
+   :alt: LaZagna Logo
+   :width: 200px
+   :align: center
 
+**LaZagna** is an open-source tool for designing and evaluating **3D FPGA architectures**.
+It supports customizable vertical interconnects, layer heterogeneity, and switch block patterns.
+LaZagna generates synthesizable RTL and bitstreams, enabling full architectural exploration
+from high-level specs to physical design.
+
+.. note::
+
+   LaZagna is under active development. New features and documentation are being added regularly.
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: About LaZagna
+
+   source/about
+
+Getting Started
+---------------
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User Guide
+
+   source/installation
+   source/usage
+   source/yaml_configuration
+
+.. .. toctree::
+..    :maxdepth: 2
+..    :caption: API Reference
+
+..    source/modules
+
+.. .. toctree::
+..    :maxdepth: 1
+..    :caption: Project
+
+..    source/contributing
+
 
